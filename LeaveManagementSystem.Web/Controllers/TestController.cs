fe72049232d97c19b2bf96c898b34b4f6b87dc94
BanValsimot <tomislav.peran1@gmail.com>
2026-03-30 10:08:27 +0200

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    //route address == Test
+    public class TestController : Controller
+    {
+        //default View == Index
+        public IActionResult Index()
+        {
+            //prepare Data
+            var data = new TestViewModel
+            {
+                Name = "Student Tomo",
+                DateOfBirth = new DateTime(1986, 11, 07)
+            };
+            return View(data);
+        }
+    }
+}
---
+++ LeaveManagementSystem.Web/Models/TestViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LeaveManagementSystem.Web.Models
+{
+    //Test == which route
+    //ViewModel == data category
+    public class TestViewModel
+    {
+        public string Name { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
